8c09d770ed800d99244643d6ecd98395d0a645d1
Abby <harbeyteejay@gmail.com>
2023-09-11 23:06:11 +0100

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a text followed by a new line
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
